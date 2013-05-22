25b86daa8723c0c05fc4260772572465bd9fc19a
EmericanX <lifebeginsfriday@gmail.com>
2013-05-21 21:57:20 -0400

    simplex vendor overhaul

---
+++ products/common_versions.mk
@@ -0,0 +1,13 @@
+# Version information used on all builds
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JDQ39E BUILD_VERSION_TAGS=release-keys USER=emericanx BUILD_UTC_DATE=$(shell date +"%s")
+
+# Additional properties
+PRODUCT_VERSION_MAJOR = 11
+PRODUCT_VERSION_MINOR = 0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.modversion=SimpleX-$(shell date +"%m-%d-%y")
+    
+#Uncomment the below lines and insert name to be displayed as Developer Id
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+#ro.build.host=EmericanX
