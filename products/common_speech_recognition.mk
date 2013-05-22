53d8f85a9d9af200a7400ee7f3812bf709bb6fb0
EmericanX <lifebeginsfriday@gmail.com>
2013-05-21 21:40:12 -0400

    SimpleX vendor overhaul

---
+++ products/common_speech_recognition.mk
@@ -0,0 +1,30 @@
+# Blobs necessary for offline speech recognition
+PRODUCT_COPY_FILES +=  \
+    vendor/simplex/proprietary/common/usr/srec/en-US/acoustic_model:system/usr/srec/en-US/acoustic_model \
+    vendor/simplex/proprietary/common/usr/srec/en-US/c_fst:system/usr/srec/en-US/c_fst \
+    vendor/simplex/proprietary/common/usr/srec/en-US/clg:system/usr/srec/en-US/clg \
+    vendor/simplex/proprietary/common/usr/srec/en-US/compile_grammar.config:system/usr/srec/en-US/compile_grammar.config \
+    vendor/simplex/proprietary/common/usr/srec/en-US/contacts.abnf:system/usr/srec/en-US/contacts.abnf \
+    vendor/simplex/proprietary/common/usr/srec/en-US/dict:system/usr/srec/en-US/dict \
+    vendor/simplex/proprietary/common/usr/srec/en-US/dictation.config:system/usr/srec/en-US/dictation.config \
+    vendor/simplex/proprietary/common/usr/srec/en-US/embed_phone_nn_model:system/usr/srec/en-US/embed_phone_nn_model \
+    vendor/simplex/proprietary/common/usr/srec/en-US/embed_phone_nn_state_sym:system/usr/srec/en-US/embed_phone_nn_state_sym \
+    vendor/simplex/proprietary/common/usr/srec/en-US/endpointer_dictation.config:system/usr/srec/en-US/endpointer_dictation.config \
+    vendor/simplex/proprietary/common/usr/srec/en-US/endpointer_voicesearch.config:system/usr/srec/en-US/endpointer_voicesearch.config \
+    vendor/simplex/proprietary/common/usr/srec/en-US/ep_acoustic_model:system/usr/srec/en-US/ep_acoustic_model \
+    vendor/simplex/proprietary/common/usr/srec/en-US/g2p_fst:system/usr/srec/en-US/g2p_fst \
+    vendor/simplex/proprietary/common/usr/srec/en-US/google_hotword_clg:system/usr/srec/en-US/google_hotword_clg \
+    vendor/simplex/proprietary/common/usr/srec/en-US/google_hotword.config:system/usr/srec/en-US/google_hotword.config \
+    vendor/simplex/proprietary/common/usr/srec/en-US/google_hotword_logistic:system/usr/srec/en-US/google_hotword_logistic \
+    vendor/simplex/proprietary/common/usr/srec/en-US/grammar.config:system/usr/srec/en-US/grammar.config \
+    vendor/simplex/proprietary/common/usr/srec/en-US/hmmsyms:system/usr/srec/en-US/hmmsyms \
+    vendor/simplex/proprietary/common/usr/srec/en-US/hotword_symbols:system/usr/srec/en-US/hotword_symbols \
+    vendor/simplex/proprietary/common/usr/srec/en-US/lintrans_model:system/usr/srec/en-US/lintrans_model \
+    vendor/simplex/proprietary/common/usr/srec/en-US/metadata:system/usr/srec/en-US/metadata \
+    vendor/simplex/proprietary/common/usr/srec/en-US/normalizer:system/usr/srec/en-US/normalizer \
+    vendor/simplex/proprietary/common/usr/srec/en-US/norm_fst:system/usr/srec/en-US/norm_fst \
+    vendor/simplex/proprietary/common/usr/srec/en-US/offensive_word_normalizer:system/usr/srec/en-US/offensive_word_normalizer \
+    vendor/simplex/proprietary/common/usr/srec/en-US/phonelist:system/usr/srec/en-US/phonelist \
+    vendor/simplex/proprietary/common/usr/srec/en-US/rescoring_lm:system/usr/srec/en-US/rescoring_lm \
+    vendor/simplex/proprietary/common/usr/srec/en-US/symbols:system/usr/srec/en-US/symbols
+
